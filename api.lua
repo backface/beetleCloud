5ca3f02546f4df3ab958b6d93c5597c1d9fd75df
Michael Aschauer <m@ash.to>
2018-09-18 12:18:55 +0200

    add social sharing

--- api.lua
+++ api.lua
@@ -171,6 +171,42 @@ app:get('/api/users/:username/projects/:projectname/image', function (self)
     end
 end)
 
+
+app:get('/api/users/:username/projects/:projectname/thumblink', function (self)
+
+    local project = Projects:find(self.params.username, self.params.projectname)
+	-- character table string
+	local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+	function dec(data)
+		data = string.gsub(data, '[^'..b..'=]', '')
+		return (data:gsub('.', function(x)
+			if (x == '=') then return '' end
+			local r,f='',(b:find(x)-1)
+			for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+			return r;
+		end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+			if (#x ~= 8) then return '' end
+			local c=0
+			for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+				return string.char(c)
+		end))
+	end
+
+    if (project) then
+		p = project.thumbnail:gsub("data:image/png;base64,","")
+		return {
+			layout = false,
+			status = 200,
+			readyState = 4,
+			content_type = "image/png",
+			dec(p)
+		}
+    else
+        return err.nonexistentProject
+    end
+end)
+
 app:match('project_list', '/api/users/:username/projects', respond_to({
     OPTIONS = cors_options,
     GET = function (self)
