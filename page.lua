5e9b045e20c679f64a3632e1964cdb6e569a5854
Michael Aschauer <m@ash.to>
2018-10-01 16:44:18 +0200

    dynamic page editing

--- page.lua
+++ page.lua
@@ -4,6 +4,15 @@
 -- poor man's dynamic pages
 
 local app = require 'app'
+local respond_to = require('lapis.application').respond_to
+local Model = require('lapis.db.model').Model
+local config = require "lapis.config".get()
+
+local Users = Model:extend('users', {
+    primary_key = { 'username' }
+})
+
+local path = "pages/"
 
 -- Endpoints
 
@@ -12,14 +21,51 @@ local app = require 'app'
 --end)
 
 app:get('/page/view/:slug', function(self)
+    self.visitor = Users:find(self.session.username)
 	slug  = self.params.slug:gsub('%W','')
-	local f=io.open("views/dynamic_pages/" .. slug .. ".etlua")
+	local f=io.open(path .. slug .. ".etlua")
 	if f~=nil then io.close(f) 
-		--return { render = "pages
 		self.page_title = slug
-		self.view = "views/dynamic_pages/" .. slug
+		self.view = path .. slug
 		return { render = "pageview" } 
 	else 
 		return { render = 'notfound' }
 	end
 end)
+
+
+app:get('/page/edit/:slug', function(self)
+	self.visitor = Users:find(self.session.username)
+	if self.visitor and self.visitor.isadmin then	
+		slug  = self.params.slug:gsub('%W','')
+		local f=io.open(path .. slug .. ".etlua")
+		if f~=nil 
+			then
+				self.content = f:read("*a")
+				f:close()
+			else self.content = ""
+		end
+		return { render = "page_edit" } 
+	else
+		return { render = 'noaccess' }
+	end		
+end)
+
+
+app:match('update_page', '/page/save/:slug', respond_to({
+    OPTIONS = cors_options,
+    POST = function (self)
+		self.visitor = Users:find(self.session.username)	
+		if self.visitor and self.visitor.isadmin then
+				
+			slug  = self.params.slug:gsub('%W','')		
+			local f=io.open(path .. slug .. ".etlua","w+")			
+			f:write(self.params.content)
+			f:close()
+
+			return { redirect_to = '/page/view/' .. slug }
+		else
+			return { render = 'noaccess' }
+		end
+    end
+}))
